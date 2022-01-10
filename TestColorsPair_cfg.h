9dffc8bfb975e2aa368d8dc06760fa452a47f34a
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-10 23:06:38 +0530

    Update TestColorsPair_cfg.h

--- TestColorsPair_cfg.h
+++ TestColorsPair_cfg.h
@@ -3,8 +3,8 @@
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET , MAX_MAJOR_COLOR};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE, MAX_MINOR_COLOR};
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
