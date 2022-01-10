25a980a7f7550dad23abe7fca710028d3b203cf2
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-10 22:45:55 +0530

    Create NumberToColorPairs.c

---
+++ NumberToColorPairs.c
@@ -0,0 +1,31 @@
+#include "ColorPairs.h"
+#include <assert.h>
+
+static ColorPair GetColorFromPairNumber(int pairNumber) 
+{
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+static void ColorPairToString(const ColorPair* colorPair, char* buffer) 
+{
+    sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],
+                            MinorColorNames[colorPair->minorColor]);
+}
+
+
+void TestNumberToPair(int pairNumber, enum MajorColor expectedMajor,
+                                      enum MinorColor expectedMinor)
+{
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
