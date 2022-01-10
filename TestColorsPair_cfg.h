24c6a65fe49adc6e4bdf966a9b6c680c24bccf8f
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-10 23:09:03 +0530

    Update TestColorsPair_cfg.h

--- TestColorsPair_cfg.h
+++ TestColorsPair_cfg.h
@@ -1,6 +1,7 @@
 #ifndef TestColorsPair_cfg_H
 #define TestColorsPair_cfg_H
 
+#define MAX_PAIR    16U
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET , MAX_MAJOR_COLOR};
