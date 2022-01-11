d77cac1b02350f8d31cfb74ee63edf40d4c28e47
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-11 11:30:42 +0530

    Update NumberToColorPairs.c

--- NumberToColorPairs.c
+++ NumberToColorPairs.c
@@ -14,7 +14,7 @@ int NumberOfMinorColors_i = sizeof(MinorColorNames) / sizeof(MinorColorNames[0])
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) 
 {
-    printf("checking %d %d",colorPair->majorColor,colorPair);
+    printf("checking %d %d",colorPair->majorColor,colorPair->minorColor);
     sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],
                             MinorColorNames[colorPair->minorColor]);
 }
