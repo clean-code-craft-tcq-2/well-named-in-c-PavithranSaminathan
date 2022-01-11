a7646f551f02acf9c62622466f06e544b01ada21
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-11 10:17:09 +0530

    Update TestColorsPair.h

--- TestColorsPair.h
+++ TestColorsPair.h
@@ -6,6 +6,9 @@
 #define MAX_PAIR    16U
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET };
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE };
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
@@ -19,8 +22,7 @@ const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET };
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE };
+
 
 extern int NumberOfMajorColors_i;
 extern int NumberOfMinorColors_i;
