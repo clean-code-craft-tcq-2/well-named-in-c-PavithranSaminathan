da05d5b3c2e715ac34f166f6af9419ea852b63bf
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-11 08:48:02 +0530

    Update TestColorsPair.c

--- TestColorsPair.c
+++ TestColorsPair.c
@@ -3,17 +3,6 @@
 int NumberOfMajorColors_i = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int NumberOfMinorColors_i = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-ColorPair GetColorFromPairNumber(int pairNumber) 
-{
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / NumberOfMinorColors_i);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % NumberOfMinorColors_i);
-    return colorPair;
-}
-
 void PrintAllPairs()
 {
     int LPairNumber;
