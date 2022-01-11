5535377c1f5498d9806b3d4b760542ea239f20e0
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-11 10:07:56 +0530

    Create TestColorsPair_cfg.c

---
+++ TestColorsPair_cfg.c
@@ -0,0 +1,11 @@
+#include "TestColorsPair_cfg.h"
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
