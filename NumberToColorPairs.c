2252c8d1ad373b37d06a4f865c7b018f5f77e1a9
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-11 13:37:05 +0530

    Update NumberToColorPairs.c

--- NumberToColorPairs.c
+++ NumberToColorPairs.c
@@ -1,41 +1,50 @@
+/*!
+ * \file         NumberToColorPairs.c
+ * \brief        This file contains functions definition to test color pair against pair number
+ */
 #include "TestColorsPair.h"
 #include <assert.h>
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 int NumberOfMajorColors_i = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int NumberOfMinorColors_i = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+/*------------------------------------------------------------------------------
+ *  FUNCTION:   void ColorPairToString(const ColorPair* colorPair, char* buffer) 
+ */
+/*! \brief      To store major and minor color names in buffer string */
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) 
 {
-    sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],
-                            MinorColorNames[colorPair->minorColor]);
+    sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
 }
 
-ColorPair GetColorFromPairNumber(int pairNumber) 
+/*------------------------------------------------------------------------------
+ *  FUNCTION:   GetColorFromPairNumber(int pairNumber)
+ */
+/*! \brief      To store major and minor color names in buffer string */
+
+ColorPair GetColorFromPairNumber(int pairNumber_i) 
 {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / NumberOfMajorColors_i);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % NumberOfMinorColors_i);
-    return colorPair;
+    ColorPair LcolorPair;
+    int LzeroBasedPairNumber = pairNumber_i - 1;
+    LcolorPair.majorColor = (enum MajorColor)(LzeroBasedPairNumber / NumberOfMajorColors_i);
+    LcolorPair.minorColor = (enum MinorColor)(LzeroBasedPairNumber % NumberOfMinorColors_i);
+    return LcolorPair;
 }
 
-void TestNumberToPair(int pairNumber, enum MajorColor expectedMajor,
-                                      enum MinorColor expectedMinor)
+/*------------------------------------------------------------------------------
+ *  FUNCTION:   TestNumberToPair(int pairNumber, enum MajorColor expectedMajor,enum MinorColor expectedMinor)
+ */
+/*! \brief      To test major and minor color name against pair number.*/
+
+void TestNumberToPair(int pairNumber, enum MajorColor expectedMajor,enum MinorColor expectedMinor)
 {
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
+    char LcolorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPair LcolorPair = GetColorFromPairNumber(pairNumber);
+    ColorPairToString(&LcolorPair, LcolorPairNames);
+    printf("Got pair %s\n", LcolorPairNames);
+    assert(LcolorPair.majorColor == expectedMajor);
+    assert(LcolorPair.minorColor == expectedMinor);
 }
