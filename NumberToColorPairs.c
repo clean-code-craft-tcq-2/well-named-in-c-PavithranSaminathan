a2083f661a90578238d88b019bf71a100e58b461
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-21 18:30:07 +0530

    Update NumberToColorPairs.c

--- NumberToColorPairs.c
+++ NumberToColorPairs.c
@@ -3,7 +3,6 @@
  * \brief        This file contains functions definition to test color pair against pair number
  */
 #include "TestColorsPair.h"
-#include <assert.h>
 
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -32,19 +31,4 @@ ColorPair GetColorFromPairNumber(int pairNumber_i)
     LcolorPair.majorColor = (enum MajorColor)(LzeroBasedPairNumber / NumberOfMajorColors_i);
     LcolorPair.minorColor = (enum MinorColor)(LzeroBasedPairNumber % NumberOfMinorColors_i);
     return LcolorPair;
-}
-
-/*------------------------------------------------------------------------------
- *  FUNCTION:   TestNumberToPair(int pairNumber, enum MajorColor expectedMajor,enum MinorColor expectedMinor)
- */
-/*! \brief      To test major and minor color name against pair number.*/
-
-void TestNumberToPair(int pairNumber, enum MajorColor expectedMajor,enum MinorColor expectedMinor)
-{
-    char LcolorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPair LcolorPair = GetColorFromPairNumber(pairNumber);
-    ColorPairToString(&LcolorPair, LcolorPairNames);
-    printf("Got pair %s\n", LcolorPairNames);
-    assert(LcolorPair.majorColor == expectedMajor);
-    assert(LcolorPair.minorColor == expectedMinor);
 }
