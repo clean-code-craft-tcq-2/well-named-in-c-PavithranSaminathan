dd96beaddece5ed32ec3572b6a95e1d6fa3b25b7
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-23 23:04:07 +0530

    Update PrintManual.c

--- PrintManual.c
+++ PrintManual.c
@@ -10,8 +10,8 @@ void Test_Number_To_ColorPair(int PairNumber , ColorPair AColorPair)
     const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
-    assert(AColorPair.MajorColor == MajorColorNames[APairNumber/5]);
-    assert(AColorPair.MinorColor == MinorColorNames[APairNumber%5]);
+    assert(AColorPair.majorColor == MajorColorNames[APairNumber/5]);
+    assert(AColorPair.minorColor == MinorColorNames[APairNumber%5]);
 }
            
            
